5a326dc03967bc4f245bac5b35c5895fac3fc801
Estêvão Fáy <estevao@ciandt.com>
2020-07-07 10:32:04 -0300

    Adding casadocodigo project

---
+++ casadocodigo/src/main/java/br/com/alura/casadocodigo/loja/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package br.com.alura.casadocodigo.loja.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index() {
+		System.out.println("Home Casa do Codigo");
+		
+		return "home";
+	}
+
+}
